aebee7fa134e073ff88c4de7eee8ed6159fc822c
mohen97 <61185425+mohen97@users.noreply.github.com>
2020-11-09 10:58:18 +0200

    Script seperate

---
+++ ven_mo.js
@@ -0,0 +1,126 @@
+var credit, additions;
+credit = 0 
+
+
+	
+function add50() {
+	additions = 0.5
+	credit = credit + additions
+	document.getElementById("demo").innerHTML = 'R' + credit;
+}
+
+function add1() {
+	additions = 1
+	credit = credit + additions
+	document.getElementById("demo").innerHTML = 'R' + credit;
+}
+
+function add2() {
+	additions = 2
+	credit = credit + additions
+	document.getElementById("demo").innerHTML = 'R' + credit;
+}
+function add5() {
+	additions = 5
+	credit = credit + additions
+	document.getElementById("demo").innerHTML = 'R' + credit;
+}
+function sprite() {
+	var stock = Number(document.getElementById('stocktable').rows[2].cells[2].innerHTML);
+	cost = 8 ;
+	if (stock == 0) {
+		window.alert("Out of Stock");
+	}
+	else if (cost>credit) {
+		window.alert("Not Enough");
+	}
+	else{
+	credit = credit - cost
+	document.getElementById("demo").innerHTML = 'R' + credit;
+	document.getElementById('stocktable').rows[2].cells[2].innerHTML = stock - 1;
+	document.getElementById("wow").innerHTML = 'Your Sprite was dispensed, you still have: R' + credit;
+	}
+}
+
+function LaysSaltVineger() {
+	var stock = Number(document.getElementById('stocktable').rows[4].cells[2].innerHTML);
+	cost = 6 ;
+	if (stock == 0) {
+		window.alert("Out of Stock");
+	}
+	else if (cost>credit) {
+		window.alert("Not Enough");
+	}
+	else{
+	credit = credit - cost;
+	document.getElementById("demo").innerHTML = 'R' + credit;
+	document.getElementById('stocktable').rows[4].cells[2].innerHTML = stock - 1;
+	alert('please wait...');
+	document.getElementById("wow").innerHTML = 'Your Lays ~ Salt & Vinegar was dispensed, you still have: R' + credit;
+
+		}
+	}
+	function LaysSourCreamandChives() {
+		var stock = Number(document.getElementById('stocktable').rows[5].cells[2].innerHTML);
+		cost = 6
+		if (stock == 0) {
+			window.alert("Out of Stock")
+		}
+		else if (cost>credit) {
+			window.alert("Not Enough")
+		}
+		else{
+		credit = credit - cost
+		document.getElementById("demo").innerHTML = 'R' + credit;
+		document.getElementById('stocktable').rows[5].cells[2].innerHTML = stock - 1;
+		document.getElementById("wow").innerHTML = 'Your Lays ~ Sour Cream & Chives was dispensed, you still have: R' + credit;
+
+		}
+	}
+	function appletizer() {
+		var stock = Number(document.getElementById('stocktable').rows[3].cells[2].innerHTML);
+		cost = 10 
+		if (stock == 0) {
+			window.alert("Out of Stock")
+		}
+		else if (cost>credit) {
+			window.alert("Not Enough")
+		}
+		else{
+		credit = credit - cost
+		document.getElementById("demo").innerHTML = 'R' + credit;
+		document.getElementById('stocktable').rows[3].cells[2].innerHTML = stock - 1;
+		document.getElementById("msg1").innerHTML = 'Your Appletizer was dispensed, you still have: R' + credit + 'remaining';
+
+		}
+	}
+	function chappies() {
+		var stock = Number(document.getElementById('stocktable').rows[1].cells[2].innerHTML);
+		cost = 0.5 
+		if (stock == 0) {
+			window.alert("Out of Stock")
+		}
+		else if (cost>credit) {
+			window.alert("Not Enough")
+		}
+		else{
+		credit = credit - cost
+		document.getElementById("demo").innerHTML = 'R' + credit;
+		document.getElementById('stocktable').rows[1].cells[2].innerHTML = stock - 1;
+		document.getElementById("msg").innerHTML = 'Your Chappie was dispensed, you still have: R' + credit + 'remaining';
+
+		}
+	}
+	function reset() {
+		document.getElementById("wow").innerHTML = 'R' + credit + ' change was returned';
+		credit = 0
+		document.getElementById("demo").innerHTML = 'R' + credit;
+	
+}
+	function restock() {
+		document.getElementById('stocktable').rows[1].cells[2].innerHTML = 20;
+		document.getElementById('stocktable').rows[2].cells[2].innerHTML = 8;
+		document.getElementById('stocktable').rows[3].cells[2].innerHTML = 8;
+		document.getElementById('stocktable').rows[4].cells[2].innerHTML = 8;
+		document.getElementById('stocktable').rows[5].cells[2].innerHTML = 8;
+	}
