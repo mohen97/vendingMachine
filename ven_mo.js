f19ab51855deb77d89da29a37a615e44dc3ea394
mohen97 <61185425+mohen97@users.noreply.github.com>
2020-11-09 15:24:01 +0200

    time reduce

--- ven_mo.js
+++ ven_mo.js
@@ -55,93 +55,92 @@ function LaysSaltVineger() {
 	credit = credit - cost;
 	document.getElementById("demo").innerHTML = 'R' + credit;
 	document.getElementById('stocktable').rows[4].cells[2].innerHTML = stock - 1;
-	alert('please wait...');
 	document.getElementById("wow").innerHTML = 'Your Lays ~ Salt & Vinegar was dispensed, you still have: R' + credit;
 
 		}
+}
+function LaysSourCreamandChives() {
+	var stock = Number(document.getElementById('stocktable').rows[5].cells[2].innerHTML);
+	cost = 6
+	if (stock == 0) {
+		window.alert("Out of Stock")
 	}
-	function LaysSourCreamandChives() {
-		var stock = Number(document.getElementById('stocktable').rows[5].cells[2].innerHTML);
-		cost = 6
-		if (stock == 0) {
-			window.alert("Out of Stock")
-		}
-		else if (cost>credit) {
-			window.alert("Not Enough")
-		}
-		else{
-		credit = credit - cost
-		document.getElementById("demo").innerHTML = 'R' + credit;
-		document.getElementById('stocktable').rows[5].cells[2].innerHTML = stock - 1;
-		document.getElementById("wow").innerHTML = 'Your Lays ~ Sour Cream & Chives was dispensed, you still have: R' + credit;
+	else if (cost>credit) {
+		window.alert("Not Enough")
+	}
+	else{
+	credit = credit - cost
+	document.getElementById("demo").innerHTML = 'R' + credit;
+	document.getElementById('stocktable').rows[5].cells[2].innerHTML = stock - 1;
+	document.getElementById("wow").innerHTML = 'Your Lays ~ Sour Cream & Chives was dispensed, you still have: R' + credit;
 
-		}
 	}
-	function appletizer() {
-		var stock = Number(document.getElementById('stocktable').rows[3].cells[2].innerHTML);
-		cost = 10 
-		if (stock == 0) {
-			window.alert("Out of Stock")
-		}
-		else if (cost>credit) {
+}
+function appletizer() {
+	var stock = Number(document.getElementById('stocktable').rows[3].cells[2].innerHTML);
+	cost = 10 
+	if (stock == 0) {
+		window.alert("Out of Stock")
+	}
+	else if (cost>credit) {
 			window.alert("Not Enough")
-		}
-		else{
-		credit = credit - cost
-		document.getElementById("demo").innerHTML = 'R' + credit;
-		document.getElementById('stocktable').rows[3].cells[2].innerHTML = stock - 1;
-		document.getElementById("wow").innerHTML = 'Your Appletizer was dispensed, you still have: R' + credit;
-
-		}
 	}
-	function chappies() {
-		var stock = Number(document.getElementById('stocktable').rows[1].cells[2].innerHTML);
-		cost = 0.5 
-		if (stock == 0) {
-			window.alert("Out of Stock")
-		}
-		else if (cost>credit) {
-			window.alert("Not Enough")
-		}
-		else{
-		credit = credit - cost
-		document.getElementById("demo").innerHTML = 'R' + credit;
-		document.getElementById('stocktable').rows[1].cells[2].innerHTML = stock - 1;
-		document.getElementById("wow").innerHTML = 'Your Chappie was dispensed, you still have: R' + credit;
+	else{
+	credit = credit - cost
+	document.getElementById("demo").innerHTML = 'R' + credit;
+	document.getElementById('stocktable').rows[3].cells[2].innerHTML = stock - 1;
+	document.getElementById("wow").innerHTML = 'Your Appletizer was dispensed, you still have: R' + credit;
 
-		}
 	}
-	function reset() {
-		displayLoader();
-		document.getElementById("wow").innerHTML = 'R' + credit + ' change was returned';
-		credit = 0
-		document.getElementById("demo").innerHTML = 'R' + credit;
+}
+function chappies() {
+	var stock = Number(document.getElementById('stocktable').rows[1].cells[2].innerHTML);
+	cost = 0.5 
+	if (stock == 0) {
+		window.alert("Out of Stock")
+	}
+	else if (cost>credit) {
+		window.alert("Not Enough")
+	}
+	else{
+	credit = credit - cost
+	document.getElementById("demo").innerHTML = 'R' + credit;
+	document.getElementById('stocktable').rows[1].cells[2].innerHTML = stock - 1;
+	document.getElementById("wow").innerHTML = 'Your Chappie was dispensed, you still have: R' + credit;
+
+	}
+}
+function reset() {
+	displayLoader();
+	document.getElementById("wow").innerHTML = 'R' + credit + ' change was returned';
+	credit = 0
+	document.getElementById("demo").innerHTML = 'R' + credit;
 	
 }
-	function restock() {
-		displayLoader();
-		document.getElementById('stocktable').rows[1].cells[2].innerHTML = 20;
-		document.getElementById('stocktable').rows[2].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[3].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[4].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[5].cells[2].innerHTML = 8;
-		document.getElementById("wow").innerHTML = 'You Are Just in Luck we are fully stocked';
+function restock() {
+	displayLoader();
+	document.getElementById('stocktable').rows[1].cells[2].innerHTML = 20;
+	document.getElementById('stocktable').rows[2].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[3].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[4].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[5].cells[2].innerHTML = 8;
+	document.getElementById("wow").innerHTML = 'You Are Just in Luck we are fully stocked';
 
 }
-	function refreshData() {
-		displayLoader();
-		document.getElementById('stocktable').rows[1].cells[2].innerHTML = 20;
-		document.getElementById('stocktable').rows[2].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[3].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[4].cells[2].innerHTML = 8;
-		document.getElementById('stocktable').rows[5].cells[2].innerHTML = 8;
-		document.getElementById("wow").innerHTML = ' Thank you for choosing Mohen!';
-		document.getElementById("demo").innerHTML = ' Enter Coins to Continue ';
+function refreshData() {
+	displayLoader();
+	document.getElementById('stocktable').rows[1].cells[2].innerHTML = 20;
+	document.getElementById('stocktable').rows[2].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[3].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[4].cells[2].innerHTML = 8;
+	document.getElementById('stocktable').rows[5].cells[2].innerHTML = 8;
+	document.getElementById("wow").innerHTML = ' Thank you for choosing Mohen!';
+	document.getElementById("demo").innerHTML = ' Enter Coins to Continue ';
 }
 		
 function displayLoader() {
-		document.getElementById("lol").style.display = "block";
-		setTimeout(function() {document.getElementById("lol").style.display = "none"}, 1200);
+	document.getElementById("lol").style.display = "block";
+	setTimeout(function() {document.getElementById("lol").style.display = "none"}, 1200);
 	
 		
-  }
+}
